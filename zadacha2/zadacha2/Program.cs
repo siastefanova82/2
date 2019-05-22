fbc7eca10dc5256d48165787c5cd01da3ca2fac4
siastefanova82 <50919796+siastefanova82@users.noreply.github.com>
2019-05-22 19:04:26 +0300

    Add files via upload

---
+++ zadacha2/zadacha2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            Console.WriteLine(Palendrom(x));
+        }
+
+        private static bool Palendrom(string x)
+        {
+            int start = 0;
+            int end = x.Length - 1;
+            for (int i = 0; i < x.Length / 2; i++)
+            {
+                if (x[start] != x[end])
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
